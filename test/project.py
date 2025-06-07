bf07af3d7be34c3c5defb26d59d9e76e68dde919
Marcelo Dantas <marceloluis257gh@gmail.com>
2025-06-07 10:27:03 -0300

    compilando projeto

---
+++ test/project.py
@@ -0,0 +1,10 @@
+project = "test_project"
+comp_flags = ["-Wall", "-Werror"]
+
+test_builds_folder = "tests"
+
+sources = [
+    "src/main.c",
+    "src/funcs/concat.c",
+    "src/structs/array.c"
+]
