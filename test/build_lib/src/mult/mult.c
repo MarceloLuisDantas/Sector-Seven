93a52b6e049e7b926c5d8d52be5dd3b0afcb99f5
Marcelo Luis <marceloluis257gh@gmail.com>
2025-06-18 19:51:59 -0300

    creating static libs

---
+++ test/build_lib/src/mult/mult.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+uint8_t mult_i8(uint8_t x, uint8_t y) {
+    return x * y;
+}
+
+uint16_t mult_i16(uint16_t x, uint16_t y) {
+    return x * y;
+}
+
+float mult_f(float x, float y) {
+    return x * y;
+}
---
+++ test/build_lib/src/sum/sum.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+uint8_t sum_i8(uint8_t x, uint8_t y) {
+    return x + y;
+}
+
+uint16_t sum_i16(uint16_t x, uint16_t y) {
+    return x + y;
+}
+
+float sum_f(float x, float y) {
+    return x + y;
+}
