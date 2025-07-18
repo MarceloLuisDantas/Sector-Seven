34ba3c2e85417e79c232a11913fd0ea4116e9e9a
Marcelo Luis <marceloluis257gh@gmail.com>
2025-07-17 22:32:57 -0300

    removendo raylib

--- sector.py
+++ sector.py
@@ -12,19 +12,18 @@ def main() :
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--init", type=str, metavar="NAME", help="creates the basic struct of a projetc")
     group.add_argument("--init-lib", type=str, metavar="NAME", help="creates the basic struct of a lib projetc")
-    group.add_argument("--init-raylib", type=str, metavar="NAME", help="creates the basic struct of a raylib projetc")
     group.add_argument("-b", "--build", action="store_true", help="builds the project")
     group.add_argument("-r", "--run", action="store_true", help="runs the project")
-    group.add_argument("-R", "--build-run", action="store_true", help="builds the project and runs the project")
+    group.add_argument("-B", "--build-run", action="store_true", help="builds the project and runs the project")
     group.add_argument("-t", "--run-test", type=str, metavar="TEST_NAME", help="run the named test")
     group.add_argument("-T", "--run-tests", action="store_true", help="run all the tests")
-    group.add_argument("-v", "--version", action="store_true", help="shows the version")
-    group.add_argument("-c", "--clean-cache", action="store_true", help="cleans the cache.json")
+    group.add_argument("--clean-cache", action="store_true", help="cleans the cache.json")
+    group.add_argument("--version", action="store_true", help="shows the version")
     group.add_argument("--valgrind", type=str, metavar="TEST_NAME_V", help="runs a test with valgrind")
 
-    parser.add_argument("-B", "--force-build", action="store_true", help="compiles all target files, ignorening cache (use with --build, --build-run, --run-test or --run-tests)")
-    parser.add_argument("-V", "--verbose", action="store_true", help="shows info about the GCC command while compiling (use with --build, --build-run, --run-test or --run-tests)")
-    parser.add_argument("--stdio", action="store_true", help="shows all the stdio made by the tests (use with --run-tests)")
+    parser.add_argument("-f", "--force-build", action="store_true", help="compiles all target files, ignorening cache (use with --build, --build-run, --run-test or --run-tests)")
+    parser.add_argument("-v", "--verbose", action="store_true", help="shows info about the GCC command while compiling (use with --build, --build-run, --run-test or --run-tests)")
+    parser.add_argument("-s", "--stdio", action="store_true", help="shows all the stdio made by the tests (use with --run-tests)")
     parser.add_argument("--shared", action="store_true", help="compiles a library to a shared object (use with --build in a lib project)")
 
     args = parser.parse_args()
@@ -37,7 +36,7 @@ def main() :
         print("--verbose can only be used with --build, --build-run, --run-test and --run-test")
         sys.exit(0)
 
-    if not any([args.run, args.build, args.init, args.init_lib, args.init_raylib, args.run_test, args.run_tests, args.version, args.build_run, args.clean_cache, args.valgrind]):
+    if not any([args.run, args.build, args.init, args.init_lib, args.run_test, args.run_tests, args.version, args.build_run, args.clean_cache, args.valgrind]):
         parser.print_help()
         sys.exit(0) 
 
@@ -60,10 +59,6 @@ def main() :
     if args.init_lib:
         init_project(args.init_lib, "lib")
         sys.exit(0)
-    
-    if args.init_raylib:
-        init_project(args.init_raylib, "ray")
-        sys.exit(0)
 
     project = load_file("./project.json")
     tests = load_file("./tests.json")
@@ -82,14 +77,13 @@ def main() :
         if (args.shared and project["type"] != "lib") :
             print("Can't use --shared in non lib projects")
         else :
-            print("alguma coisa")
             build_project(project, chache_log, args.force_build, args.verbose, args.shared)
     elif args.build_run :
         ptype = project["type"]
         if (ptype == "lib") :
             print("Cant run libs projects")
         else :
-            ok = build_project(project, chache_log, args.force_build, args.verbose)
+            ok = build_project(project, chache_log, args.force_build, args.verbose, args.shared)
             if (ok) :
                 print("")
                 run(name, project)
